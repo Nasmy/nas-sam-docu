ef240284b66168beab71ff396e07670c1dfd00a0
nasmy <nasmy@keeneye.solutions>
2024-03-06 23:41:31 +0530

    Add data

--- deploy.sh
+++ deploy.sh
@@ -19,9 +19,10 @@ rsync -av functions/layers/ functions/prompts_ner/ --exclude=requirements.txt
 
 sam validate --region us-east-1
 sam build -c -p --use-container
+sam package --template-file template.yaml --s3-bucket nas-dd-files-digest-bucket --output-template-file packaged.yaml
 sam deploy \
 --profile nas-docudive \
---template-file template.yaml
+--template-file packaged.yaml \
 --stack-name nas-docudive-backend \
 --region us-east-1 \
 --no-confirm-changeset \
